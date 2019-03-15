a5190fac13d06b4fac987eb052939d2799944c2b
Jacob Emcken <jacob@emcken.dk>
2019-03-15 16:45:22 +0100

    Implement missing escaping for generating Line Protocol
    See https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference#special-characters

--- src/dk/emcken/influxdb_client/convert.clj
+++ src/dk/emcken/influxdb_client/convert.clj
@@ -11,17 +11,27 @@
   (:require [clojure.string :as str]
             [dk.emcken.influxdb-client.precision :as precision]))
 
-(defn val->str
+(defn format-data-type
+  "See https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference#data-types"
   [v]
   (cond
     (float? v) v
     (boolean? v) (if v "t" "f")
     (int? v) (str v "i")
-    :else (str "\"" v "\"")))
+    :else (str "\"" (str/escape v {\" "\\\""}) "\"")))
+
+(defn escape
+  "See https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference#special-characters"
+  [s]
+  (str/escape s {\, "\\,"
+                 \= "\\="
+                 \space "\\ "}))
 
 (defn key-val->str
-  [m]
-  (map #(str (name (key %)) "=" (val->str (val %))) m))
+  [val->str m]
+  (map #(str (escape (name (key %)))
+             "="
+             (val->str (val %))) m))
 
 (defmulti ->nano
   "Takes an instant and returns it as nano seconds since epoch."
@@ -46,8 +56,9 @@
   ([point]
    (point->line point ::precision/ns))
   ([{:keys [measurement fields tags time] :as point} precision]
-   (str (str/join "," (conj (key-val->str tags) measurement))
+   (str (str/join "," (conj (key-val->str escape tags)
+                            (str/escape measurement {\, "\\," \space "\\ "})))
         " "
-        (str/join "," (key-val->str fields))
+        (str/join "," (key-val->str format-data-type fields))
         (when time
           (str " " (adjust-precision time precision))))))
