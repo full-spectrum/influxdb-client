a914c8d72aba11e4c37a4ed0e95be917d7f007f1
Jacob Emcken <jacob@emcken.dk>
2019-05-30 22:28:42 +0200

    Use a more permissive license

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Clojure InfluxDB client. Supporting InfluxDB 1.7 and without
   introducing an unnecessary extra dependency like the Java client."
   :url "https://github.com/full-spectrum/clj-influxdb-client/"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[clj-http "3.9.1"]
                  [metosin/jsonista "0.2.2"]]
   :profiles {:dev {:dependencies [[clojure.java-time "0.3.2"]
